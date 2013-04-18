aa5ff55a7cbbdf7c8b4d3afa2cfc0c54cea3c39b
Nam Phung <nampq@exoplatform.com>
2013-04-18 09:56:32 +0700

    refactor SamplePortlet.java

--- src/main/java/org/exoplatform/portlet/SamplePortlet.java
+++ src/main/java/org/exoplatform/portlet/SamplePortlet.java
@@ -31,29 +31,29 @@ public class SamplePortlet extends GenericPortlet {
   public void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException {
     String sYourName = (String) request.getParameter("yourname");
     if (sYourName != null) {
-        PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/jsp/hello.jsp");
-        prd.include(request, response);
+        renderResponse(request, response, "/jsp/hello.jsp");
     } else {
-        PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/jsp/welcome.jsp");
-        prd.include(request, response);
+        renderResponse(request, response, "/jsp/welcome.jsp");
     }
   }
   
   protected void doHelp(RenderRequest rRequest, RenderResponse rResponse) throws PortletException, IOException {
-    rResponse.setContentType("text/html");
-    PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/jsp/help.jsp");
-    prd.include(rRequest, rResponse);
+    renderResponse(rRequest, rResponse, "/jsp/help.jsp");
   }
   
   protected void doEdit(RenderRequest rRequest, RenderResponse rResponse) throws PortletException, IOException {
-    rResponse.setContentType("text/html");
-    PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher("/jsp/edit.jsp");
-    prd.include(rRequest, rResponse);
+    renderResponse(rRequest, rResponse, "/jsp/edit.jsp");
   }
 
   public void processAction(ActionRequest aRequest, ActionResponse aResponse) throws PortletException, IOException {
     String sYourname = (String) aRequest.getParameter("yourname");
     aResponse.setRenderParameter("yourname", sYourname);
+  }
+  
+  private void renderResponse(RenderRequest rRequest, RenderResponse rResponse, String path) throws PortletException, IOException {
+    rResponse.setContentType("text/html");
+    PortletRequestDispatcher prd = getPortletContext().getRequestDispatcher(path);
+    prd.include(rRequest, rResponse);
   }
   
 }
